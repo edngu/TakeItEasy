c22def8f9785de61bf75d1516aae341b784397c5
savillegas58 <savillegas58@gmail.com>
2025-01-15 06:21:27 -0800

    Fixed up the Quiz View and NotesTableCell

--- TakeItEasy/Controller/QuizViewController.swift
+++ TakeItEasy/Controller/QuizViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+
+    @IBOutlet weak var quizInfoBackdropView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        quizInfoBackdropView.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
     }
     
@@ -28,16 +30,27 @@ class QuizViewController: UIViewController {
 
 }
 
-extension QuizViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension QuizViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 128, height: 128)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "QuizCell", for: indexPath) as! QuizCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "quizCell", for: indexPath) as! QuizCollectionViewCell
+        cell.layer.cornerRadius = 20
+        //cell.quizImage.image = UIImage(systemName: "trash.fill")
+        cell.quizBackDropView.layer.cornerRadius = 20
+        
         
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "questionSegue", sender: nil)
+    }
     
 }
