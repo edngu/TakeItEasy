3851de3210ae6ef439c5e179d01bbf173eb69980
Edward <edwardnguyen248@gmail.com>
2025-01-31 10:23:15 -0500

    images for result screen

--- TakeItEasy/Controller/ResultsViewController.swift
+++ TakeItEasy/Controller/ResultsViewController.swift
@@ -17,25 +17,44 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var numWrong: UILabel!
     @IBOutlet weak var numCorrect: UILabel!
     @IBOutlet weak var username: UILabel!
-
+    @IBOutlet weak var resultImage: UIImageView!
+    
     var quiz : Quiz?
+    
+    enum ResultEmojis: String {
+        case Perfect = "happy-emoji"
+        case Pass = "smile-emoji"
+        case Fail = "sad-emoji"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setLabels()
+        setImage(numCorrect: quiz?.score)
         username.text = GlobalData.shared.signedInAccount?.email
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setImage(numCorrect: Int?) {
+        guard let correct = numCorrect else {
+            return
+        }
+        
+        var imageFile : String?
+        
+        if correct >= 5 {
+            imageFile = Bundle.main.path(forResource: ResultEmojis.Perfect.rawValue, ofType: "png")
+        } else if correct >= 3 {
+            imageFile = Bundle.main.path(forResource: ResultEmojis.Pass.rawValue, ofType: "png")
+        } else {
+            imageFile = Bundle.main.path(forResource: ResultEmojis.Fail.rawValue, ofType: "png")
+        }
+        
+        if let image = imageFile {
+            resultImage.image = UIImage(named: image)
+        }
     }
-    */
     
     func setupUI(){
         backdropView0.layer.cornerRadius = 30
